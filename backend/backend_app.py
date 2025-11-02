048f8d7aa38c9d6f677959e2962337ba05024772
jasonbdt <ja.bladt@gmail.com>
2025-11-02 03:21:17 +0100

    feat(backend): Add create post function

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,6 +13,33 @@ POSTS = [
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def create_post():
+    req_body = request.get_json()
+
+    if not req_body['title'] and not req_body['content']:
+        return jsonify({
+            "message": "Invalid request body: 'title' and 'content' are missing"
+        }), 400
+    elif not req_body['title']:
+        return jsonify({
+            "message": "Invalid request body: 'title' is missing"
+        }), 400
+    elif not req_body['content']:
+        return jsonify({
+            "message": "Invalid request body: 'content' is missing"
+        }), 400
+
+    new_post = {
+        "id": POSTS[-1]['id'] + 1,
+        "title": req_body['title'],
+        "content": req_body['content']
+    }
+    POSTS.append(new_post)
+
+    return jsonify(new_post), 201
 
 
 if __name__ == '__main__':
